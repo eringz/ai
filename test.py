0fc0198d1194f232bb0e2fcc864aded225e4a036
unknown <ron.garcia.santos@gmail.com>
2025-12-12 03:32:47 +0800

    testing audio

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-import requests
+import requests, webbrowser
+from playsound import playsound
 
 def get_pronounciation(word):
     url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
@@ -10,12 +11,23 @@ def get_pronounciation(word):
         return
     
     data = res.json()[0]
-    print(f"Data: {data}")
+    # print(f"Data: {data}")
     
     # print("WORD:", data["word"])
+    # print("Phonetic:", data["phonetic"])
+    # print(f"Phonetics: {data["phonetics"][0]["audio"]}")
     
-    # for p in data.get("phoenetics", []):
-    #     if "text" in p:
-    #         print("")
+    audio_url = data["phonetics"][0]["audio"]
+    print(audio_url)
+    webbrowser.open(audio_url)
+    
+    
+    
+    for p in data.get("phoenetics", []):
+        if "text" in p:
+            print("")
 
-get_pronounciation("computer")
+
+while True:
+    word = input("Enter the word: ")
+    get_pronounciation(word)

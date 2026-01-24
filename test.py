5332cca1bb13f8be90e6825bb239ac3cdf65746d
unknown <ron.garcia.santos@gmail.com>
2026-01-24 11:05:29 +0800

    update

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.service import Service
+from dotenv import load_dotenv
 
+load_dotenv()
 
 service = Service(executable_path="")
 driver = webdriver.Chrome(service=service)
+
+
